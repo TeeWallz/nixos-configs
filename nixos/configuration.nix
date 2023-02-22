11de64470bde704f65f8367323119c3bbc9f1cdc
Tom Waller <tom.waller@versent.com.au>
2023-02-22 23:07:19 +0100

    Resolving disco shenanigans

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -6,7 +6,7 @@
 imports =
   [ # Include the results of the hardware scan.
     ./hardware-configuration.nix
-    inputs.disko.nixosModules.disko
+    # inputs.disko.nixosModules.disko
     # "${
     #     (
     #       fetchTarball 
