0b618e857e60e20a7a9e7a2ec8fb5acd9acab73d
Tom Waller <tom.waller@versent.com.au>
2023-02-22 23:28:45 +0100

    Resolving disco shenanigans

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -79,10 +79,10 @@ imports =
   # boot.loader.grub.efiInstallAsRemovable = true;
 
   boot.loader = {
-  efi = {
-    canTouchEfiVariables = true;
-    efiSysMountPoint = "/boot/efi"; # ← use the same mount point here.
-  };
+  # efi = {
+  #   canTouchEfiVariables = true;
+  #   efiSysMountPoint = "/boot/efi"; # ← use the same mount point here.
+  # };
   grub = {
      efiSupport = true;
      #efiInstallAsRemovable = true; # in case canTouchEfiVariables doesn't work for your system
