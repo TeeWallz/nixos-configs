7ef5fe9c7414d653b1ed2011203159554b0ea35f
Tom Waller <tom.waller@versent.com.au>
2023-02-23 10:27:46 +0100

    Resolving disco shenanigans

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -83,11 +83,16 @@ imports =
   #   canTouchEfiVariables = true;
   #   efiSysMountPoint = "/boot/efi"; # ← use the same mount point here.
   # };
-  grub = {
-     efiSupport = true;
-     #efiInstallAsRemovable = true; # in case canTouchEfiVariables doesn't work for your system
-     device = "nodev";
-  };
+  # grub = {
+  #    enable = true;
+  #    useOSProber = true;
+  #    efiSupport = true;
+  #    #efiInstallAsRemovable = true; # in case canTouchEfiVariables doesn't work for your system
+  #    devices = [ "nodev" ];
+  # };
+  systemd-boot = {
+    enable = true;
+  }
 };
 
 
