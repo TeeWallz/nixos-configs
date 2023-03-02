b0489ca1c9bb4c31af2f90b04d3c3849caed210c
Tom Waller <tom.waller@versent.com.au>
2023-03-02 12:45:48 +0100

    Resolving disco shenanigans

--- v1-zfs-working/setup_vm.sh
+++ v1-zfs-working/setup_vm.sh
@@ -2,44 +2,54 @@
 
 set –e
 
-export DISK='/dev/disk/by-id/ata-QEMU_HARDDISK_QM00003'
-export INST_PARTSIZE_SWAP=4
+# STEP 1 - SET UP PARTITITONS
+# GOAL:
+#   PARTITION 5: BIOS BOOT?     (24KiB - 1MiB)
+#   PARTITION 1: EFI            (1MiB - 1GiB)
+#   PARTITION 2: ZFS BOOT POOL  (1GiB - 4GiB)
+#   PARTITION 4: LINUX SWAP     (4GiB - 4+INST_PARTSIZE_SWAP GiB)
+#   PARTITION 3: DATA ROOT      (Fill)
+
+
 
 for i in ${DISK}; do
 
-# wipe flash-based storage device to improve
-# performance.
-# ALL DATA WILL BE LOST
-# blkdiscard -f $i
+    # wipe flash-based storage device to improve
+    # performance.
+    # ALL DATA WILL BE LOST
+    # blkdiscard -f $i
 
-sgdisk --zap-all $i
+    sgdisk --zap-all $i
 
-# PART1 - EFI system partition
-sgdisk -n1:1M:+1G -t1:EF00 $i
+    # PART1 - EFI system partition
+    sgdisk -n1:1M:+1G -t1:EF00 $i
 
-# PART2 - ZFS Boot Pool - Solaris Boot Type
-sgdisk -n2:0:+4G -t2:BE00 $i
+    # PART2 - ZFS Boot Pool - Solaris Boot Type
+    sgdisk -n2:0:+4G -t2:BE00 $i
 
-# PART4 - linux swap
-sgdisk -n4:0:+${INST_PARTSIZE_SWAP}G -t4:8200 $i
+    # PART4 - linux swap
+    sgdisk -n4:0:+${INST_PARTSIZE_SWAP}G -t4:8200 $i
 
-# PART3 - ZPOOL
-if test -z $INST_PARTSIZE_RPOOL; then
-    sgdisk -n3:0:0   -t3:BF00 $i
-else
-    sgdisk -n3:0:+${INST_PARTSIZE_RPOOL}G -t3:BF00 $i
-fi
+    # PART3 - ZPOOL
+    if test -z $INST_PARTSIZE_RPOOL; then
+        sgdisk -n3:0:0   -t3:BF00 $i
+    else
+        sgdisk -n3:0:+${INST_PARTSIZE_RPOOL}G -t3:BF00 $i
+    fi
 
-# PART5 - BIOS BOOT
-sgdisk -a1 -n5:24K:+1000K -t5:EF02 $i
+    # PART5 - BIOS BOOT
+    sgdisk -a1 -n5:24K:+1000K -t5:EF02 $i
 
-sync && udevadm settle && sleep 3
+    # Wait for all disk tasks to complete
+    sync && udevadm settle && sleep 3
 
-cryptsetup open --type plain --key-file /dev/random $i-part4 ${i##*/}-part4
-mkswap /dev/mapper/${i##*/}-part4
-swapon /dev/mapper/${i##*/}-part4
+    # Activate swap - TODO - Why cryptswap open?
+    cryptsetup open --type plain --key-file /dev/random $i-part4 ${i##*/}-part4
+    mkswap /dev/mapper/${i##*/}-part4
+    swapon /dev/mapper/${i##*/}-part4
 done
 
+# Create ZFS Boot pool
 zpool create \
     -o compatibility=grub2 \
     -o ashift=12 \
@@ -58,6 +68,7 @@ zpool create \
        printf "$i-part2 ";
       done)
 
+# Create ZFS Root pool
 zpool create \
     -o ashift=12 \
     -o autotrim=on \
@@ -75,6 +86,25 @@ zpool create \
       printf "$i-part3 ";
      done)
 
+# ZFS WEBSITE POOLS/DATASETS
+# Datasets:
+#  zpool
+#    nixos
+#      root     -> /mnt/
+#      home     -> /mnt/home
+#      var      -> 
+#      var/lib  -> 
+#      var/log  -> 
+#      empty    ->               (Snapshotted empty, labelled @start)
+#  bpool
+#    nixos
+#      root     -> /mnt/boot
+
+
+
+
+
+# Create nixos dataset on rpool/nixos
 zfs create \
     -o canmount=off \
     -o mountpoint=none \
@@ -83,19 +113,26 @@ zfs create \
     -o keyformat=passphrase \
     rpool/nixos
 
-
+# Create and mount root dataset
 zfs create -o mountpoint=legacy     rpool/nixos/root
 mount -t zfs rpool/nixos/root /mnt/
+
+# Create and mount home
 zfs create -o mountpoint=legacy rpool/nixos/home
 mkdir /mnt/home
 mount -t zfs  rpool/nixos/home /mnt/home
+
+# Create var datasets
 zfs create -o mountpoint=legacy  rpool/nixos/var
 zfs create -o mountpoint=legacy rpool/nixos/var/lib
 zfs create -o mountpoint=legacy rpool/nixos/var/log
+
+# Create boot datasets
 zfs create -o mountpoint=none bpool/nixos
 zfs create -o mountpoint=legacy bpool/nixos/root
 mkdir /mnt/boot
 mount -t zfs bpool/nixos/root /mnt/boot
+
 zfs create -o mountpoint=legacy rpool/nixos/empty
 zfs snapshot rpool/nixos/empty@start
 
