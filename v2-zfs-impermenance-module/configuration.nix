f00eb44cea24e5f39a2e5054313b019bd5090cef
Tom Waller <tom.waller@versent.com.au>
2023-03-04 17:21:51 +0100

    Nice stuff to config

--- v2-zfs-impermenance-module/configuration.nix
+++ v2-zfs-impermenance-module/configuration.nix
@@ -32,33 +32,46 @@ in {
   # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
 
   # Set your time zone.
-  # time.timeZone = "Europe/Amsterdam";
+  time.timeZone = "Europe/Warsaw";
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Select internationalisation properties.
-  # i18n.defaultLocale = "en_US.UTF-8";
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  #   useXkbConfig = true; # use xkbOptions in tty.
-  # };
+  i18n.defaultLocale = "en_GB.UTF-8";
 
-  # Configure keymap in X11
-  # services.xserver.layout = "us";
-  # services.xserver.xkbOptions = {
-  #   "eurosign:e";
-  #   "caps:escape" # map caps to escape.
-  # };
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "pl_PL.UTF-8";
+    LC_IDENTIFICATION = "pl_PL.UTF-8";
+    LC_MEASUREMENT = "pl_PL.UTF-8";
+    LC_MONETARY = "pl_PL.UTF-8";
+    LC_NAME = "pl_PL.UTF-8";
+    LC_NUMERIC = "pl_PL.UTF-8";
+    LC_PAPER = "pl_PL.UTF-8";
+    LC_TELEPHONE = "pl_PL.UTF-8";
+    LC_TIME = "pl_PL.UTF-8";
+  };
 
   # Enable CUPS to print documents.
-  # services.printing.enable = true;
+  services.printing.enable = true;
 
-  # Enable sound.
-  # sound.enable = true;
-  # hardware.pulseaudio.enable = true;
+# Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
 
   # Enable touchpad support (enabled default in most desktopManager).
 
@@ -72,6 +85,21 @@ in {
   #   ];
   # };
 
+ # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the Desktop Environment.
+  services.xserver.displayManager.sddm.enable = true;
+  #services.xserver.desktopManager.plasma5.enable = true;
+  services.xserver.desktopManager.xfce.enable = true;
+
+    # Configure keymap in X11
+  services.xserver = {
+    layout = "pl";
+    xkbVariant = "";
+  };
+
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs;
@@ -81,20 +109,27 @@ in {
       ## The Nano editor is also installed by default.
       #   wget
       mg
+      wget
+      git
+      spice-vdagent
+      tmux
+      tmuxp
+      git
+      vscode
     ];
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  programs.mtr.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
 
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  services.openssh.enable = false;
+  services.openssh.enable = true;
   users.users.root = {
     ##hash: mkpasswd -m SHA-512 -s
     initialHashedPassword = PLACEHOLDER_FOR_ROOT_PWD_HASH;
@@ -138,6 +173,12 @@ in {
   boot.extraModulePackages = [ ];
 
   fileSystems = {
+    "/boot" = {
+      device = "bpool/nixos/root";
+      fsType = "zfs";
+      options = [ "X-mount.mkdir" ];
+    };
+
     "/" = {
       device = "rpool/nixos/local/root";
       fsType = "zfs";
@@ -167,12 +208,6 @@ in {
     #   fsType = "zfs";
     #   options = [ "X-mount.mkdir" ];
     # };
-
-    "/boot" = {
-      device = "bpool/nixos/root";
-      fsType = "zfs";
-      options = [ "X-mount.mkdir" ];
-    };
   } // (builtins.listToAttrs (map (diskName: {
     name = zfsRoot.mirroredEfi + diskName + zfsRoot.partitionScheme.efiBoot;
     value = {
@@ -224,6 +259,8 @@ in {
   # networking.interfaces.enp1s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  nixpkgs.config.allowUnfree = true;
+
   hardware.cpu.intel.updateMicrocode =
     lib.mkDefault config.hardware.enableRedistributableFirmware;
   hardware.cpu.amd.updateMicrocode =
