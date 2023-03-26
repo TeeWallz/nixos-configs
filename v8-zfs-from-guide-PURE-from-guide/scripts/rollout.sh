976a06d9586998f69f3d21bc183e52649a234a3d
Tom Waller <tom.waller@versent.com.au>
2023-03-26 21:50:03 +0200

    Haha

--- v8-zfs-from-guide-PURE-from-guide/scripts/rollout.sh
+++ v8-zfs-from-guide-PURE-from-guide/scripts/rollout.sh
@@ -4,7 +4,9 @@ cd /tmp
 curl -o /tmp/nixfiles.zip -L "https://github.com/TeeWallz/nixos-configs/archive/refs/heads/main.zip"
 unzip /tmp/nixfiles.zip
 
-cd /tmp/nixos-configs-main/v8-zfs-from-guide-PURE-from-guide/dotfiles-flake/
+cd /tmp/nixos-configs-main
+cd v8-zfs-from-guide-PURE-from-guide/dotfiles-flake/
+
 cp -r * /mnt/etc/nixos
 
 
