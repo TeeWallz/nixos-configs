e5f2979da08c529fd18fef0db763f0ab55c57ddf
Tom Waller <tom.waller@versent.com.au>
2023-04-02 21:59:02 +0200

    First try at merge

--- common/optional/zfs_boot_and_data/default.nix
+++ common/optional/zfs_boot_and_data/default.nix
@@ -3,8 +3,8 @@ with lib;
 
 let cfg = config.boot;
 in {
-  # imports = [ ./grub-for-arm64/grub.nix ];
-  boot = {
+  imports = [ ./grub-for-arm64/grub.nix ];
+  options.boot = {
     enable = mkOption {
       description = "Enable root on ZFS support";
       type = types.bool;
