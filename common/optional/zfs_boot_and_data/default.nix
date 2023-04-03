15524847a2e0ba2ea7f2f74054f0657c1e15a935
Tom Waller <tom.waller@versent.com.au>
2023-04-03 19:23:59 +0200

    First try at merge

--- common/optional/zfs_boot_and_data/default.nix
+++ common/optional/zfs_boot_and_data/default.nix
@@ -153,19 +153,19 @@ in {
         };
       };
     }
-    (mkIf (cfg.system == "aarch64-linux") {
-      boot.aarch64Grub = {
-        devices = [ "nodev" ];
-        efiInstallAsRemovable = cfg.removableEfi;
-        # used patched grub for arm64
-        enable = true;
-        version = 2;
-        copyKernels = true;
-        efiSupport = true;
-        zfsSupport = true;
-        extraInstallCommands = config.boot.loader.grub.extraInstallCommands;
-      };
-    })
+    # (mkIf (cfg.system == "aarch64-linux") {
+    #   boot.aarch64Grub = {
+    #     devices = [ "nodev" ];
+    #     efiInstallAsRemovable = cfg.removableEfi;
+    #     # used patched grub for arm64
+    #     enable = true;
+    #     version = 2;
+    #     copyKernels = true;
+    #     efiSupport = true;
+    #     zfsSupport = true;
+    #     extraInstallCommands = config.boot.loader.grub.extraInstallCommands;
+    #   };
+    # })
     (mkIf (cfg.system != "aarch64-linux") { boot.loader.grub.enable = true; })
     (mkIf cfg.sshUnlock.enable {
       boot.initrd = {
