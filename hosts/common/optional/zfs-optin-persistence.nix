9cdf692f563883add9c6278ca6da117338d4c222
Tom Waller <tom.waller@versent.com.au>
2023-04-10 00:10:52 +0200

    First try at merge

--- hosts/common/optional/zfs-optin-persistence.nix
+++ hosts/common/optional/zfs-optin-persistence.nix
@@ -31,24 +31,28 @@ in {
       device = "bpool/nixos/root";
       fsType = "zfs";
       options = [ "X-mount.mkdir" ];
+      neededForBoot = true;
     };
 
     "/" = {
       device = "rpool/nixos/local/root";
       fsType = "zfs";
       options = [ "X-mount.mkdir" ];
+      neededForBoot = true;
     };
 
     "/nix" = {
       device = "rpool/nixos/local/nix";
       fsType = "zfs";
       options = [ "X-mount.mkdir" ];
+      neededForBoot = true;
     };
 
     "/persist" = {
       device = "rpool/nixos/persist";
       fsType = "zfs";
       options = [ "X-mount.mkdir" ];
+      neededForBoot = true;
     };
 
     # "/var/lib" = {
